dc69b92f3fb79ee21c9b19d6ec484e6b6260c8b0
Asheer Rizvi <asheer.vil@gmail.com>
2022-02-06 17:39:36 +0530

    Exercise 02: Extra Credit 1.

--- src/__tests__/exercise/02.js
+++ src/__tests__/exercise/02.js
@@ -21,11 +21,11 @@ test('counter increments and decrements when the buttons are clicked', () => {
   const [decrement, increment] = container.querySelectorAll('button')
   const message = container.firstChild.querySelector('div')
 
-  expect(message.textContent).toBe('Current count: 0')
+  expect(message).toHaveTextContent('Current count: 0')
 
   // 🐨 replace the next two statements with `fireEvent.click(button)`
   fireEvent.click(increment)
-  expect(message.textContent).toBe('Current count: 1')
+  expect(message).toHaveTextContent('Current count: 1')
   fireEvent.click(decrement)
-  expect(message.textContent).toBe('Current count: 0')
+  expect(message).toHaveTextContent('Current count: 0')
 })
