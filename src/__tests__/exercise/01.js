cbb05ea10e46878fcec5b537bc428860b8032a89
Asheer Rizvi <asheer.vil@gmail.com>
2022-02-06 16:35:16 +0530

    Exercise 01: Simple test with ReactDOM.

--- src/__tests__/exercise/01.js
+++ src/__tests__/exercise/01.js
@@ -5,24 +5,45 @@ import * as React from 'react'
 import ReactDOM from 'react-dom'
 import Counter from '../../components/counter'
 
+beforeEach(() => {
+  document.body.innerHTML = ''
+})
+
 test('counter increments and decrements when the buttons are clicked', () => {
   // 🐨 create a div to render your component to (💰 document.createElement)
-  //
+  const divEl = document.createElement('div')
   // 🐨 append the div to document.body (💰 document.body.append)
-  //
+  document.body.append(divEl)
   // 🐨 use ReactDOM.render to render the <Counter /> to the div
+  ReactDOM.render(<Counter />, divEl)
   // 🐨 get a reference to the increment and decrement buttons:
   //   💰 div.querySelectorAll('button')
+  const [decrement, increment] = divEl.querySelectorAll('button')
   // 🐨 get a reference to the message div:
   //   💰 div.firstChild.querySelector('div')
-  //
+  const message = divEl.firstChild.querySelector('div')
   // 🐨 expect the message.textContent toBe 'Current count: 0'
+  expect(message.textContent).toBe('Current count: 0')
   // 🐨 click the increment button (💰 increment.click())
+  const incrementClickEvent = new MouseEvent('click', {
+    bubbles: true,
+    cancelable: true,
+    button: 0,
+  })
+  increment.dispatchEvent(incrementClickEvent)
   // 🐨 assert the message.textContent
+  expect(message.textContent).toBe('Current count: 1')
   // 🐨 click the decrement button (💰 decrement.click())
+  const decrementClickEvent = new MouseEvent('click', {
+    bubbles: true,
+    cancelable: true,
+    button: 0,
+  })
+  decrement.dispatchEvent(decrementClickEvent)
   // 🐨 assert the message.textContent
-  //
+  expect(message.textContent).toBe('Current count: 0')
   // 🐨 cleanup by removing the div from the page (💰 div.remove())
+  divEl.remove()
   // 🦉 If you don't cleanup, then it could impact other tests and/or cause a memory leak
 })
 
