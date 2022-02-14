5c797252df6a90f99f188f3c5a1a81cae37fce0c
Asheer Rizvi <asheer.vil@gmail.com>
2022-02-14 17:14:28 +0530

    Exercise 03: Avoid implementation details.

--- src/__tests__/exercise/03.js
+++ src/__tests__/exercise/03.js
@@ -3,15 +3,16 @@
 
 import * as React from 'react'
 // 🐨 add `screen` to the import here:
-import {render, fireEvent} from '@testing-library/react'
+import {screen, render, fireEvent} from '@testing-library/react'
 import Counter from '../../components/counter'
 
 test('counter increments and decrements when the buttons are clicked', () => {
-  const {container} = render(<Counter />)
+  render(<Counter />)
   // 🐨 replace these with screen queries
   // 💰 you can use `getByText` for each of these (`getByRole` can work for the button too)
-  const [decrement, increment] = container.querySelectorAll('button')
-  const message = container.firstChild.querySelector('div')
+  const decrement = screen.getByRole('button', {name: /decrement/i});
+  const increment = screen.getByRole('button', {name: /increment/i});
+  const message = screen.getByText(/current count/i);
 
   expect(message).toHaveTextContent('Current count: 0')
   fireEvent.click(increment)
