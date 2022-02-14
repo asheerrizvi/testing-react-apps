34ec0fd1d1ac27256e34ef692e635aa1241489ac
Asheer Rizvi <asheer.vil@gmail.com>
2022-02-14 18:05:45 +0530

    Exercise 03: Extra Credit 1.

--- src/__tests__/exercise/03.js
+++ src/__tests__/exercise/03.js
@@ -3,7 +3,8 @@
 
 import * as React from 'react'
 // 🐨 add `screen` to the import here:
-import {screen, render, fireEvent} from '@testing-library/react'
+import {screen, render} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Counter from '../../components/counter'
 
 test('counter increments and decrements when the buttons are clicked', () => {
@@ -15,8 +16,8 @@ test('counter increments and decrements when the buttons are clicked', () => {
   const message = screen.getByText(/current count/i);
 
   expect(message).toHaveTextContent('Current count: 0')
-  fireEvent.click(increment)
+  userEvent.click(increment)
   expect(message).toHaveTextContent('Current count: 1')
-  fireEvent.click(decrement)
+  userEvent.click(decrement)
   expect(message).toHaveTextContent('Current count: 0')
 })
