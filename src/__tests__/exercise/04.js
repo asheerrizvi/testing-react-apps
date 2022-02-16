e2acb126d148a3a7b29ebe42628b118d40cb9113
Asheer Rizvi <asheer.vil@gmail.com>
2022-02-16 20:01:52 +0530

    Exercise 04: Extra Credit 4.

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -2,18 +2,17 @@
 // http://localhost:3000/login
 
 import * as React from 'react'
+import {build, fake} from '@jackfranklin/test-data-bot'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
-const {faker} = require('@faker-js/faker')
 
-function buildLoginForm(overrides) {
-  return {
-    username: faker.internet.userName(),
-    password: faker.internet.password(),
-    ...overrides
+const buildLoginForm = build({
+  fields: {
+    username: fake(faker => faker.internet.userName()),
+    password: fake(faker => faker.internet.password()),
   }
-}
+})
 
 test('submitting the form calls onSubmit with username and password', () => {
   const handleSubmit = jest.fn()
