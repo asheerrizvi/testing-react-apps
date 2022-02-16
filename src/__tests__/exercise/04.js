6bbb1664d685025b3c82ed0035fa81ef08481fc4
Asheer Rizvi <asheer.vil@gmail.com>
2022-02-16 19:23:09 +0530

    Exercise 04: Extra Credit 2.

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -5,12 +5,19 @@ import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
+const {faker} = require('@faker-js/faker')
+
+function buildLoginForm() {
+  return {
+    username: faker.internet.userName(),
+    password: faker.internet.password(),
+  }
+}
 
 test('submitting the form calls onSubmit with username and password', () => {
   const handleSubmit = jest.fn()
   render(<Login onSubmit={handleSubmit} />)
-  const username = 'Kylo Ren'
-  const password = 'thedarklord'
+  const {username, password} = buildLoginForm()
 
   const submit = screen.getByRole('button', {
     name: /submit/i,
