1359d5778f3a3e0e0ac2b95797e4e90e2555972b
Asheer Rizvi <asheer.vil@gmail.com>
2022-02-27 15:14:03 +0530

    Exercise 05: Extra Credit 4.

--- src/__tests__/exercise/05.js
+++ src/__tests__/exercise/05.js
@@ -11,6 +11,7 @@ import {build, fake} from '@jackfranklin/test-data-bot'
 import {setupServer} from 'msw/node'
 import Login from '../../components/login-submission'
 import {handlers} from '../../test/server-handlers'
+import { rest } from 'msw'
 
 const buildLoginForm = build({
   fields: {
@@ -30,6 +31,7 @@ const server = setupServer(...handlers)
 
 beforeAll(() => server.listen())
 afterAll(() => server.close())
+afterEach(() => server.resetHandlers())
 
 test(`logging in displays the user's username`, async () => {
   render(<Login />)
@@ -78,4 +80,31 @@ test(`logging in without a password shows an error`, async () => {
   expect(screen.getByRole('alert').textContent).toMatchInlineSnapshot(
     `"password required"`,
   )
+})
+
+test(`unknown server error displays the error message`, async () => {
+  const testErrorMsg = 'something went strong!';
+  server.use(
+    rest.post(
+      // note that it's the same URL as our app-wide handler
+      // so this will override the other.
+      'https://auth-provider.example.com/api/login',
+      async (req, res, ctx) => {
+        // your one-off handler here
+        return res(
+          ctx.delay(0),
+          ctx.status(500),
+          ctx.json({message: testErrorMsg}),
+        )
+      },
+    ),
+  )
+
+  render(<Login />)
+
+  userEvent.click(screen.getByRole('button', {name: /submit/i}))
+
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
+
+  expect(screen.getByRole('alert')).toHaveTextContent(testErrorMsg)
 })
