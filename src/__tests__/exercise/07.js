66f9b14edff7632f960af57fe8416e4da2ae2485
Asheer Rizvi <asheer.vil@gmail.com>
2022-04-01 18:02:45 +0530

    Exercise 07: Context and Custom Render Method.

--- src/__tests__/exercise/07.js
+++ src/__tests__/exercise/07.js
@@ -8,12 +8,15 @@ import EasyButton from '../../components/easy-button'
 
 test('renders with the light styles for the light theme', () => {
   // 🐨 uncomment all of this code and your test will be busted on the next line:
-  // render(<EasyButton>Easy</EasyButton>)
-  // const button = screen.getByRole('button', {name: /easy/i})
-  // expect(button).toHaveStyle(`
-  //   background-color: white;
-  //   color: black;
-  // `)
+  const Wrapper = ({children}) => (
+    <ThemeProvider initialTheme="light">{children}</ThemeProvider>
+  )
+  render(<EasyButton>Easy</EasyButton>, {wrapper: Wrapper})
+  const button = screen.getByRole('button', {name: /easy/i})
+  expect(button).toHaveStyle(`
+    background-color: white;
+    color: black;
+  `)
   //
   // 🐨 update the `render` call above to use the wrapper option using the
   // ThemeProvider
