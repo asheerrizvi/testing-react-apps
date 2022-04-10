cc407a61c5f0de5d47c78d0e1cfb54fd4d23e4f5
Asheer Rizvi <asheer.vil@gmail.com>
2022-04-10 22:20:53 +0530

    Exercise 08: Extra Credit 2.

--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -5,19 +5,49 @@ import * as React from 'react'
 import {act, render} from '@testing-library/react'
 import useCounter from '../../components/use-counter'
 
-test('exposes the count and increment/decrement functions', () => {
-  let result
-  function TestComponent(props) {
-    result = useCounter(props)
+function setup(...args) {
+  const returnVal = {}
+  function TestComponent() {
+    Object.assign(returnVal, useCounter(...args))
     return null
   }
-
   render(<TestComponent />)
-  expect(result.count).toBe(0)
-  act(() => result.increment())
-  expect(result.count).toBe(1)
-  act(() => result.decrement())
-  expect(result.count).toBe(0)
+  return returnVal
+}
+
+
+test('exposes the count and increment/decrement functions', () => {
+  const counterData = setup()
+
+  expect(counterData.count).toBe(0)
+  act(() => counterData.increment())
+  expect(counterData.count).toBe(1)
+  act(() => counterData.decrement())
+  expect(counterData.count).toBe(0)
+})
+
+test('allows customization of the initial count', () => {
+  const counterData = setup({
+    initialCount: 1
+  })
+
+  expect(counterData.count).toBe(1)
+  act(() => counterData.increment())
+  expect(counterData.count).toBe(2)
+  act(() => counterData.decrement())
+  expect(counterData.count).toBe(1)
+})
+
+test('allows customization of the step', () => {
+  const counterData = setup({
+    step: 2
+  })
+
+  expect(counterData.count).toBe(0)
+  act(() => counterData.increment())
+  expect(counterData.count).toBe(2)
+  act(() => counterData.decrement())
+  expect(counterData.count).toBe(0)
 })
 
 /* eslint no-unused-vars:0 */
