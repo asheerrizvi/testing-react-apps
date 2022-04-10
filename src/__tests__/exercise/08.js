c4ce3b73588ec129a6eedfeeccf2bda017a734c1
Asheer Rizvi <asheer.vil@gmail.com>
2022-04-10 22:30:07 +0530

    Exercise 08: Extra Credit 3.

--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -2,52 +2,42 @@
 // http://localhost:3000/counter-hook
 
 import * as React from 'react'
-import {act, render} from '@testing-library/react'
+import {act, renderHook} from '@testing-library/react-hooks'
 import useCounter from '../../components/use-counter'
-
-function setup(...args) {
-  const returnVal = {}
-  function TestComponent() {
-    Object.assign(returnVal, useCounter(...args))
-    return null
-  }
-  render(<TestComponent />)
-  return returnVal
-}
 
 
 test('exposes the count and increment/decrement functions', () => {
-  const counterData = setup()
+  const {result} = renderHook(() => useCounter())
 
-  expect(counterData.count).toBe(0)
-  act(() => counterData.increment())
-  expect(counterData.count).toBe(1)
-  act(() => counterData.decrement())
-  expect(counterData.count).toBe(0)
+  expect(result.current.count).toBe(0)
+  act(() => result.current.increment())
+  expect(result.current.count).toBe(1)
+  act(() => result.current.decrement())
+  expect(result.current.count).toBe(0)
 })
 
 test('allows customization of the initial count', () => {
-  const counterData = setup({
+  const {result} = renderHook(() => useCounter({
     initialCount: 1
-  })
+  }))
 
-  expect(counterData.count).toBe(1)
-  act(() => counterData.increment())
-  expect(counterData.count).toBe(2)
-  act(() => counterData.decrement())
-  expect(counterData.count).toBe(1)
+  expect(result.current.count).toBe(1)
+  act(() => result.current.increment())
+  expect(result.current.count).toBe(2)
+  act(() => result.current.decrement())
+  expect(result.current.count).toBe(1)
 })
 
 test('allows customization of the step', () => {
-  const counterData = setup({
+  const {result} = renderHook(() => useCounter({
     step: 2
-  })
+  }))
 
-  expect(counterData.count).toBe(0)
-  act(() => counterData.increment())
-  expect(counterData.count).toBe(2)
-  act(() => counterData.decrement())
-  expect(counterData.count).toBe(0)
+  expect(result.current.count).toBe(0)
+  act(() => result.current.increment())
+  expect(result.current.count).toBe(2)
+  act(() => result.current.decrement())
+  expect(result.current.count).toBe(0)
 })
 
 /* eslint no-unused-vars:0 */
