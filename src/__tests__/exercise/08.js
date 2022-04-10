2f4131f4ddeb7dad0c0104e5539485e407a87db2
Asheer Rizvi <asheer.vil@gmail.com>
2022-04-10 17:23:03 +0530

    Exercise 08: Testing Custom Hooks.

--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -12,11 +12,33 @@ import useCounter from '../../components/use-counter'
 // 💰 here's how to use the hook:
 // const {count, increment, decrement} = useCounter()
 
+function Count() {
+  const {count, increment, decrement} = useCounter()
+
+  return (
+    <>
+      <p>Count: {count}</p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </>
+  )
+}
+
 test('exposes the count and increment/decrement functions', () => {
   // 🐨 render the component
   // 🐨 get the elements you need using screen
   // 🐨 assert on the initial state of the hook
   // 🐨 interact with the UI using userEvent and assert on the changes in the UI
+  render(<Count />)
+  const increment = screen.getByRole('button', {name: /increment/i})
+  const decrement = screen.getByRole('button', {name: /decrement/i})
+  const count = screen.getByText(/count/i)
+
+  expect(count).toHaveTextContent('Count: 0')
+  userEvent.click(increment)
+  expect(count).toHaveTextContent('Count: 1')
+  userEvent.click(decrement)
+  expect(count).toHaveTextContent('Count: 0')
 })
 
 /* eslint no-unused-vars:0 */
